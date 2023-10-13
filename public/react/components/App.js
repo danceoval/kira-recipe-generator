8f7b5ad39e8e9e49c8cd03d9cb988c2a2155a88b
Dan Sohval <daniel.sohval@gmail.com>
2023-10-12 20:32:45 -0400

    adds react component

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'; // You'll need to install axios
 
 export const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [recipe, setRecipe] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-	return (
-		<div>	
-			<h1>Kira Snack Generator</h1>
-		</div>
-	)
-}
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const apiKey = 'YOUR_OPENAI_API_KEY';
+    const prompt = `I have the following ingredients: ${inputValue.trim()}. What snack recipe can I make?`;
+
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        prompt,
+        max_tokens: 150, // Adjust the max_tokens as needed
+      }, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      });
+
+      const recommendedRecipe = response.data.choices[0].text;
+
+      setRecipe(recommendedRecipe);
+    } catch (error) {
+      console.error('Error fetching data from OpenAI:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Kira Snack Recipe Generator</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter ingredients (comma-separated):
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+        </label>
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? <span>Generating Recipe...</span> : <span>Get Recipe</span>}
+        </button>
+      </form>
+      {recipe && !isLoading && (
+        <div>
+          <h2>Recommended Snack Recipe:</h2>
+          <p>{recipe}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
