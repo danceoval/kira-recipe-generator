f526e19d0edc3988e2dd2f9dd1db3ccc173a8389
Dan Sohval <daniel.sohval@gmail.com>
2023-10-16 16:36:12 -0400

    adds react components

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios'; // You'll need to install axios
 
+import {OPEN_AI_KEY} from '../../../secrets';
+
+
 export const App = () => {
   const [inputValue, setInputValue] = useState('');
+  const [ingredients, setIngredients] = useState([]);
   const [recipe, setRecipe] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
@@ -10,46 +14,91 @@ export const App = () => {
     setInputValue(event.target.value);
   };
 
+  const handleAddIngredient = () => {
+    if (inputValue.trim() !== '') {
+      setIngredients([...ingredients, inputValue.trim()]);
+      setInputValue('');
+    }
+  };
+
+  const handleRemoveIngredient = (index) => {
+    const updatedIngredients = [...ingredients];
+    updatedIngredients.splice(index, 1);
+    setIngredients(updatedIngredients);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const apiKey = 'YOUR_OPENAI_API_KEY';
-    const prompt = `I have the following ingredients: ${inputValue.trim()}. What snack recipe can I make?`;
+    // Set your OpenAI API key here
+    const apiKey = OPEN_AI_KEY;
 
+    // Create a prompt using the user's ingredients
+    const prompt = `I have the following ingredients: ${ingredients.join(', ')}. What snack recipe can I make?`;
+
+    // Send a request to the OpenAI API
     try {
       setIsLoading(true);
 
-      const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-        prompt,
-        max_tokens: 150, // Adjust the max_tokens as needed
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          { role: "system", content: prompt}
+        ]
       }, {
         headers: {
-          Authorization: `Bearer ${apiKey}`,
+          "Authorization": `Bearer ${apiKey}`,
+          "Content-Type": "application/json"
         },
       });
 
-      const recommendedRecipe = response.data.choices[0].text;
+      console.log("POSTING", response)
 
+      const recommendedRecipe = response.data.choices[0].message.content;
+      setIsLoading(false);
       setRecipe(recommendedRecipe);
     } catch (error) {
       console.error('Error fetching data from OpenAI:', error);
-    } finally {
-      setIsLoading(false);
-    }
+    } 
   };
 
   return (
     <div>
-      <h1>Kira Snack Recipe Generator</h1>
+      <h1>Snack Recipe Recommendations</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Enter ingredients (comma-separated):
-          <input type="text" value={inputValue} onChange={handleInputChange} />
-        </label>
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? <span>Generating Recipe...</span> : <span>Get Recipe</span>}
-        </button>
+        <div>
+          <label>
+            Add Ingredient:
+            <input type="text" value={inputValue} onChange={handleInputChange} />
+          </label>
+          <button type="button" onClick={handleAddIngredient}>
+            Add Ingredient
+          </button>
+        </div>
+        {ingredients.length > 0 && (
+          <div>
+            <h2>Ingredients List:</h2>
+            <ul>
+              {ingredients.map((ingredient, index) => (
+                <li key={index}>
+                  {ingredient}
+                  <button type="button" onClick={() => handleRemoveIngredient(index)}>
+                    Remove Ingredient
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {!isLoading && ingredients.length > 0 && (
+          <button type="submit">
+            Get Recipe
+          </button>
+        )}
       </form>
+      {isLoading && (
+        <p>Loading recipe...</p>
+      )}
       {recipe && !isLoading && (
         <div>
           <h2>Recommended Snack Recipe:</h2>
@@ -59,4 +108,3 @@ export const App = () => {
     </div>
   );
 };
-
